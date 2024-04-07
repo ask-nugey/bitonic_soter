37d80bac9232cb0251adda843c358945ef9354e7
ask-nugey <mischief.moi@gmail.com>
2024-04-07 21:08:47 +0900

    ✨ feat: sort関数を実装

--- src/fisrt.rs
+++ src/fisrt.rs
@@ -1,5 +1,10 @@
-pub fn first(x: &mut [u32], up: bool) {
-    unimplemented!()
+pub fn sort(x: &mut [u32], up: bool) {
+    if x.len() > 1 {
+        let middle_point = x.len() / 2;
+        sort(&mut x[..middle_point], true);
+        sort(&mut x[middle_point..], true);
+        sub_sort(x, up);
+    }
 }
 
 fn sub_sort(x: &mut [u32], up: bool) {
